bdd0f5a7d46f1e7848b165f1e53b2a0573f88e0a
Andrey Malyar <a_malayr@mail.ru>
2026-01-10 22:11:55 +0500

    chore: начальная настройка проекта

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.ts',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
